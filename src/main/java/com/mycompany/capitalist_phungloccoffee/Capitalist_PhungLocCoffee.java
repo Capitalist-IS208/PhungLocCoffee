841d76116312649d419beb7a93248b62e4eacd5d
ailaconmeohar <buiphandeplao06@gmail.com>
2026-04-11 22:44:03 +0700

    Khoi tao cau truc Project Phung Loc Coffee

---
+++ src/main/java/com/mycompany/capitalist_phungloccoffee/Capitalist_PhungLocCoffee.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.capitalist_phungloccoffee;
+
+/**
+ *
+ * @author 24521
+ */
+public class Capitalist_PhungLocCoffee {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
